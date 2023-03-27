1bb71de334c5d53d74d70ec375028b0d60ec4d4f
PetarHarambasa <croman.3365@gmail.com>
2023-03-27 08:59:29 +0200

    initialized commit and added template for registration page

---
+++ app/src/main/java/hr/medick/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package hr.medick
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.medick.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
