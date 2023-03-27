b2ac3f3ea7c4f8b115d7e773ed6519d4224a16c1
PetarHarambasa <croman.3365@gmail.com>
2023-03-27 20:49:51 +0200

    implemented connection with backend and data saving from register page

--- app/src/main/java/hr/medick/RegisterActivity.kt
+++ app/src/main/java/hr/medick/RegisterActivity.kt
@@ -1,9 +1,14 @@
 package hr.medick
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import hr.medick.databinding.ActivityRegisterBinding
+import hr.medick.model.Osoba
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import java.io.IOException
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
@@ -22,23 +27,69 @@ class RegisterActivity : AppCompatActivity() {
             val lozinka = binding.lozinkaEditText.text
             val ponovljenaLozinka = binding.ponovnoLozinkaEditText.text
 
+
+            // ip of device that is vrtiti backend
+            // žično ipadesa: 192.168.0.126 žično ne radi
+            // bežično ipadrsa: 192.168.1.3 radi ko mašina
+            val url = "http://192.168.1.3:8080/mobileRegister"
+            val osoba = Osoba(
+                null,
+                ime.toString(),
+                prezime.toString(),
+                email.toString(),
+                telefon.toString(),
+                adresaStanovanja.toString(),
+                lozinka.toString()
+            )
+
             // Validate user input
             if (ime.isEmpty() || prezime.isEmpty() || email.isEmpty() || telefon.isEmpty() || adresaStanovanja.isEmpty() || lozinka.isEmpty() || ponovljenaLozinka.isEmpty()) {
                 Toast.makeText(this, "Molim, popunite sva polja", Toast.LENGTH_SHORT).show()
-            } else if(!email.contains("@")){
+            } else if (!email.contains("@")) {
                 Toast.makeText(this, "Molim, upisite svoj email", Toast.LENGTH_SHORT).show()
-            }
-            else if (lozinka.trim() != ponovljenaLozinka.trim()) {
+            } else if (lozinka.trim() != ponovljenaLozinka.trim()) {
                 Toast.makeText(this, "Molim, potvrdite lozinku", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                Toast.makeText(this, "Registration successful (Fake)", Toast.LENGTH_SHORT).show()
+            } else {
+                saveData(url, osoba)
                 clearInputs()
             }
         }
     }
 
-    fun clearInputs(){
+    private fun saveData(url: String, osoba: Osoba) {
+        val client = OkHttpClient()
+
+        val json = Gson().toJson(osoba)
+        val requestBody = RequestBody.create("application/json".toMediaTypeOrNull(), json)
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+
+        val gfgThread = Thread {
+            try {
+                // Your network activity
+                val result = client.newCall(request).enqueue(object : Callback{
+                    override fun onFailure(call: Call, e: IOException) {
+                        println(e)
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        println(response)
+                    }
+                })
+                println(result)
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        gfgThread.start()
+    }
+
+    fun clearInputs() {
         binding.imeEditText.text.clear()
         binding.prezimeEditText.text.clear()
         binding.emailEditText.text.clear()
---
+++ app/src/main/java/hr/medick/model/Osoba.kt
@@ -0,0 +1,9 @@
+package hr.medick.model
+
+data class Osoba (
+    val id: Long? = null,
+    val ime: String? = null, val prezime: String? = null,
+    val email: String? = null, val telefon: String? = null,
+    val adresaStanovanja: String? = null, val lozinka: String? = null){
+
+}
